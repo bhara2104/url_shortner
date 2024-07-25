1ba3314222440ddcb022f64b83f1f55dc6bebfee
bharath <bharathbk2121@gmail.com>
2024-07-25 09:29:05 +0530

    URL shortener base dao

--- src/main/java/com/application/url_shortner/dao/UrlProviderDao.java
+++ src/main/java/com/application/url_shortner/dao/UrlProviderDao.java
@@ -29,30 +29,75 @@ public class UrlProviderDao {
 
     public static void saveUrl(String originalUrl, String shortenedUrl) {
         String sql = "insert into url_shortened(original_url, shortened_url, count) values (?, ?, 0)";
-        try (PreparedStatement preparedStatement = getConnectionFromConnectionPool().prepareStatement(sql); Jedis jedis = getJedisConnection()) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try (Jedis jedis = getJedisConnection()) {
+            connection = getConnectionFromConnectionPool();
+            preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setString(1, originalUrl);
-            preparedStatement.setString(2,shortenedUrl);
+            preparedStatement.setString(2, shortenedUrl);
             preparedStatement.executeUpdate();
             jedis.set(shortenedUrl, originalUrl);
         } catch (Exception e) {
             System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (preparedStatement != null) preparedStatement.close();
+                removeConnectionFromConnectionPoolInstance(connection);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
-    public static ShortenedUrl getUrl(String shortenedUrl){
+    public static ShortenedUrl getUrl(String shortenedUrl) {
         String sql = "select * from url_shortened where shortened_url = ?";
-        ResultSet resultSet ;
+        ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
         ShortenedUrl shortenedUrl1 = new ShortenedUrl();
-        try(PreparedStatement preparedStatement = getConnectionFromConnectionPool().prepareStatement(sql); Jedis jedis = getJedisConnection()){
-            preparedStatement.setString(1, shortenedUrl);
-            resultSet = preparedStatement.executeQuery();
-            if(resultSet.next()){
-                shortenedUrl1 = new ShortenedUrl(resultSet.getString("original_string"), resultSet.getInt("count"));
-                jedis.set(shortenedUrl, String.valueOf(shortenedUrl1));
+        try (Jedis jedis = getJedisConnection()) {
+            if (jedis.get(shortenedUrl) != null) {
+                shortenedUrl1 = new ShortenedUrl(shortenedUrl, jedis.get(shortenedUrl));
+            } else {
+                Connection connection = null;
+                try {
+                    connection = getConnectionFromConnectionPool();
+                    preparedStatement = connection.prepareStatement(sql);
+                    preparedStatement.setString(1, shortenedUrl);
+                    resultSet = preparedStatement.executeQuery();
+                    if (resultSet.next()) {
+                        shortenedUrl1 = new ShortenedUrl(resultSet.getString("original_string"), resultSet.getInt("count"));
+                        jedis.set(shortenedUrl, shortenedUrl1.getOriginalUrl());
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    try {
+                        if (resultSet != null) {
+                            resultSet.close();
+                            preparedStatement.close();
+                        }
+                        removeConnectionFromConnectionPoolInstance(connection);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
             }
-        }catch (Exception e){
-            System.out.println(e.getMessage());
         }
         return shortenedUrl1;
+    }
+
+    public static void updateAccessCount(String shortUrl) {
+        String sql = "update url_shortened set access_count = access_count+1 where shortened_url = ?";
+        PreparedStatement preparedStatement = null;
+        Connection connection = null;
+        try {
+            connection = getConnectionFromConnectionPool();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, shortUrl);
+            preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
